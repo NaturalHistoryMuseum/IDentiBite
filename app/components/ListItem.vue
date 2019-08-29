92070356e799b82b4f57cd127c5371dedfead2cb
benscott.co.uk <ben@benscott.co.uk>
2019-08-29 18:44:13 +0100

    Add list item

---
+++ app/components/ListItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <GridLayout columns="120, *">
+    <Image
+      height="85"
+      width="120"
+      col="0"
+      :src="fileName | imageAssetPath"
+      stretch="aspectFill"
+    />
+    <StackLayout col="1" verticalAlignment="middle" height="85">
+      <Label :text="title" />
+      <Label class="subtitle" v-if="subtitle">
+        <FormattedString>
+          <Span :text="subtitle" fontAttributes="Italic" />
+        </FormattedString>
+      </Label>
+    </StackLayout>
+  </GridLayout>
+</template>
+
+<script>
+export default {
+  props: {
+    fileName: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+
+label {
+  padding-left: 30px;
+}
+image {
+  border-color: $white;
+}
+
+StackLayout {
+  border-color: $light-grey;
+}
+
+StackLayout,
+image {
+  border-bottom-width: 1;
+}
+
+.subtitle {
+  font-style: italic;
+  color: $softer-dark-grey;
+}
+</style>
