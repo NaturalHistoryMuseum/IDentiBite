f8aa7fc46753a14c1df2735d36a6cee9ffb1d65c
benscott.co.uk <ben@benscott.co.uk>
2019-08-29 18:45:21 +0100

    Reusable warning component

---
+++ app/components/Warning.vue
@@ -0,0 +1,22 @@
+<template>
+  <StackLayout orientation="horizontal" class="m-t-15 m-b-15 body-item">
+    <Label
+      class="fa red"
+      :text="'fa-exclamation-circle' | fonticon"
+      width="50"
+    />
+    <Label :text="text" class="red" textWrap="true" />
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  props: ["text"]
+};
+</script>
+
+<style lang="scss" scoped>
+label {
+  //   vertical-align: top;
+}
+</style>
