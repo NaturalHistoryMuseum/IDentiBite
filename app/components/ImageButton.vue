810b79a407f08646fe45e09e783fe4ea0bf1b845
benscott.co.uk <ben@benscott.co.uk>
2019-09-01 19:32:48 +0100

    Add image button

---
+++ app/components/ImageButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <StackLayout
+    orientation="horizontal"
+    @tap="$emit('tap')"
+    class="btn btn-icon"
+  >
+    <Label class="fa icon" :text="icon | fonticon" />
+    <Label
+      class="label"
+      :text="label"
+      verticalAlignment="center"
+      width="100%"
+    ></Label>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  props: ["icon", "label"]
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  padding: 15 0 15 15;
+  .icon {
+    font-size: 20px;
+  }
+  .label {
+    padding-left: 15;
+  }
+}
+</style>
