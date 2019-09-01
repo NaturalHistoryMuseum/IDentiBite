956ae1308f75c50f0c63fb0d3aee2b64f3484681
benscott.co.uk <ben@benscott.co.uk>
2019-09-01 19:32:31 +0100

    Add about page

---
+++ app/pages/About.vue
@@ -0,0 +1,22 @@
+]<template>
+  <Page>
+    <Header page-title="About" />
+    <StackLayout>
+      <StackLayout class="hr m-b-1" height="5"></StackLayout>
+      <ScrollView> </ScrollView>
+    </StackLayout>
+  </Page>
+</template>
+
+
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+// .page-content {
+//   padding: 40px;
+// }
+</style>
+
