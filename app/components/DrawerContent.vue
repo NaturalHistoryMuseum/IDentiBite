aefe7c54a1a3e13ecf86f486a3362e6ad7505c92
benscott.co.uk <ben@benscott.co.uk>
2019-09-03 16:51:10 +0100

    Fix drawer header

--- app/components/DrawerContent.vue
+++ app/components/DrawerContent.vue
@@ -1,38 +1,78 @@
-<template lang="html">
-<StackLayout rows="auto, *" class="sidedrawer sidedrawer-left">
-    <Label id="sidedrawer-header" class="sidedrawer-header" text="IDentiBite" verticalAlignment="middle" />
+<template>
+  <StackLayout rows="auto, *" class="sidedrawer sidedrawer-left">
+    <StackLayout
+      id="sidedrawer-header"
+      class="sidedrawer-header vertical-align"
+    >
+      <Label text="IDentiBite" />
+    </StackLayout>
 
     <ScrollView row="1" class="sidedrawer-content">
-        <StackLayout>
-            <GridLayout columns="auto, *" :class="'sidedrawer-list-item' + (selectedPage === 'home' ? ' selected': '')" @tap="$goto('home')">
-                <Label col="0" text.decode="&#xf015;" class="fa"></Label>
-                <Label col="1" text="Home"></Label>
-            </GridLayout>
+      <StackLayout>
+        <GridLayout
+          columns="auto, *"
+          :class="
+            'sidedrawer-list-item' +
+              (selectedPage === 'home' ? ' selected' : '')
+          "
+          @tap="$goto('home')"
+        >
+          <Label col="0" text.decode="&#xf015;" class="fa"></Label>
+          <Label col="1" text="Home"></Label>
+        </GridLayout>
 
-            <GridLayout columns="auto, *" :class="'sidedrawer-list-item' + (selectedPage === 'identify' ? ' selected': '')" @tap="$goto('identify')">
-                <Label col="0" text.decode="&#xf002;" class="fa"></Label>
-                <Label col="1" text="Identify"></Label>
-            </GridLayout>
+        <GridLayout
+          columns="auto, *"
+          :class="
+            'sidedrawer-list-item' +
+              (selectedPage === 'identify' ? ' selected' : '')
+          "
+          @tap="$goto('identify')"
+        >
+          <Label col="0" text.decode="&#xf002;" class="fa"></Label>
+          <Label col="1" text="Identify"></Label>
+        </GridLayout>
 
-            <GridLayout columns="auto, *" :class="'sidedrawer-list-item' + (selectedPage === 'explore' ? ' selected': '')" @tap="$goto('explore')">
-                <Label col="0" text.decode="&#xf188;" class="fa"></Label>
-                <Label col="1" text="Explore"></Label>
-            </GridLayout>
+        <GridLayout
+          columns="auto, *"
+          :class="
+            'sidedrawer-list-item' +
+              (selectedPage === 'explore' ? ' selected' : '')
+          "
+          @tap="$goto('explore')"
+        >
+          <Label col="0" text.decode="&#xf188;" class="fa"></Label>
+          <Label col="1" text="Explore"></Label>
+        </GridLayout>
 
-            <GridLayout columns="auto, *" :class="'sidedrawer-list-item' + (selectedPage === 'treatment' ? ' selected': '')" @tap="$goto('treatment')">
-                <Label col="0" text.decode="&#xf21e;" class="fa"></Label>
-                <Label col="1" text="Treatment"></Label>
-            </GridLayout>
+        <GridLayout
+          columns="auto, *"
+          :class="
+            'sidedrawer-list-item' +
+              (selectedPage === 'treatment' ? ' selected' : '')
+          "
+          @tap="$goto('treatment')"
+        >
+          <Label col="0" text.decode="&#xf21e;" class="fa"></Label>
+          <Label col="1" text="Treatment"></Label>
+        </GridLayout>
 
-            <StackLayout class="hr-light"></StackLayout>
+        <StackLayout class="hr-light"></StackLayout>
 
-            <GridLayout columns="auto, *" :class="'sidedrawer-list-item' + (selectedPage === 'about' ? ' selected': '')" @tap="$goto('about')">
-                <Label col="0" text.decode="&#xf05a;" class="fa"></Label>
-                <Label col="1" text="About"></Label>
-            </GridLayout>
-        </StackLayout>
+        <GridLayout
+          columns="auto, *"
+          :class="
+            'sidedrawer-list-item' +
+              (selectedPage === 'about' ? ' selected' : '')
+          "
+          @tap="$goto('about')"
+        >
+          <Label col="0" text.decode="&#xf05a;" class="fa"></Label>
+          <Label col="1" text="About"></Label>
+        </GridLayout>
+      </StackLayout>
     </ScrollView>
-</StackLayout>
+  </StackLayout>
 </template>
 
 <script>
@@ -70,11 +110,16 @@ $sidedrawer-list-icon-size: 20;
   .sidedrawer-header {
     @include header-bar;
     padding: 0;
-    padding-top: 0;
+    padding-top: 3;
     padding-bottom: 0;
     font-size: $font-size-large;
     margin-bottom: 0;
     height: 46;
+    // label {
+    //   vertical-align: middle;
+    //   height: 100%;
+    //   //   padding: 10 0 30 0;
+    // }
   }
 
   .sidedrawer-content {
