5700f306584b85ba6a76c93a9ec14754d220c753
benscott.co.uk <ben@benscott.co.uk>
2019-09-06 14:15:59 +0100

    Full stops

--- app/components/TreatmentReferences.vue
+++ app/components/TreatmentReferences.vue
@@ -9,58 +9,65 @@
     <Label
       textWrap="true"
       class="body"
-      text="Elliot, A., Cross, K., Smith G. and et al. (2006) The association between impetigo, insect bites and air temperature: a retrospective 5-year study (1999-2003) using morbidity data collected from a sentinel general practice network database.Family practice. 23(5), 490-496"
+      text="Elliot, A., Cross, K., Smith G. and et al. (2006) The association between impetigo, insect bites and air temperature: a retrospective 5-year study (1999-2003) using morbidity data collected from a sentinel general practice network database.Family practice. 23(5), 490-496."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Golden, D., Moffitt, J., Nicklas, R. and et al. (2011) Stinging insect hypersensitivity: a practice parameter update 2011.Journal of Allergy and Clinical Immunology. 127(4), 852-854"
+      text="Golden, D., Moffitt, J., Nicklas, R. and et al. (2011) Stinging insect hypersensitivity: a practice parameter update 2011.Journal of Allergy and Clinical Immunology. 127(4), 852-854."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Järvinen KM (2009)Allergic reactions to stinging and biting insects and arachnids. Pediatr Ann 2009; 38: 199–209"
+      text="Järvinen KM (2009)Allergic reactions to stinging and biting insects and arachnids. Pediatr Ann 2009; 38: 199–209."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Juckett, G. (2013) Arthropod bites. American Family Physician. 88(12), 841-847"
+      text="Juckett, G. (2013) Arthropod bites. American Family Physician. 88(12), 841-847."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Larry I. Goodyer, Ashley M. Croft, Steve P. Frances, Nigel Hill, Sarah J. Moore, Sangoro P. Onyango, Mustapha Debboun, Expert Review of the Evidence Base for Arthropod Bite Avoidance, Journal of Travel Medicine, Volume 17, Issue 3, 1 May 2010, Pages 182–192"
+      text="Larry I. Goodyer, Ashley M. Croft, Steve P. Frances, Nigel Hill, Sarah J. Moore, Sangoro P. Onyango, Mustapha Debboun, Expert Review of the Evidence Base for Arthropod Bite Avoidance, Journal of Travel Medicine, Volume 17, Issue 3, 1 May 2010, Pages 182–192."
     />
     <Label
       textWrap="true"
       class="body"
-      text="NICE (2011) Anaphylaxis: assessment and referral after emergency treatment. CG134.  National Institute for Health and Care Excellence.. www.nice.org.uk"
+      text="NICE (2011) Anaphylaxis: assessment and referral after emergency treatment. CG134.  National Institute for Health and Care Excellence. www.nice.org.uk."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Przybilla, B. and and Rueff, F. (2012) Insect stings: clinical features and management. Deutsches Arzteblatt international.109(13), 238-248"
+      text="Przybilla, B. and and Rueff, F. (2012) Insect stings: clinical features and management. Deutsches Arzteblatt international.109(13), 238-248."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Working Group of the Resuscitation Council (UK) (2008)  Emergency treatment of anaphylactic reactions: guidelines for healthcare providers. Resuscitation Council (UK).. www.resus.org.uk"
+      text="Working Group of the Resuscitation Council (UK) (2008)  Emergency treatment of anaphylactic reactions: guidelines for healthcare providers. Resuscitation Council (UK).. www.resus.org.uk."
     />
     <Label
       textWrap="true"
       class="body"
-      text="Goddard, J., & Zhou, L. (2007). Physician’s Guide to Arthropods of Medical Importance, 5th Edition. Emerging Infectious Diseases, 13(9), 1442. doi:10.3201/eid1309.070706"
+      text="Goddard, J., & Zhou, L. (2007). Physician’s Guide to Arthropods of Medical Importance, 5th Edition. Emerging Infectious Diseases, 13(9), 1442. doi:10.3201/eid1309.070706."
     />
-    <Label
-      textWrap="true"
-      class="body"
-      text="https://www.nhs.uk/conditions/insect-bites-and-stings/"
-    />
-    <Label
-      textWrap="true"
-      class="body"
-      text="https://cks.nice.org.uk/insect-bites-and-stings#!topicSummary"
-    />
+    <Anchor href="https://www.nhs.uk/conditions/insect-bites-and-stings/">
+      <Label
+        textWrap="true"
+        class="body"
+        text="https://www.nhs.uk/conditions/insect-bites-and-stings/"
+      />
+    </Anchor>
+
+    <Anchor
+      href="https://cks.nice.org.uk/insect-bites-and-stings#!topicSummary"
+    >
+      <Label
+        textWrap="true"
+        class="body"
+        text="https://cks.nice.org.uk/insect-bites-and-stings#!topicSummary"
+      />
+    </Anchor>
   </StackLayout>
 </template>
 
